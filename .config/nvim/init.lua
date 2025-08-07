5a684041edfa821334ebc05498820949a3c5a5d8
taco <tylerchall16@gmail.com>
2025-08-07 15:08:06 -0500

    moved everything to .config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,55 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = "yes"
+vim.o.termguicolors = true
+vim.o.wrap = false
+vim.o.tabstop = 2
+vim.o.swapfile = false
+vim.g.mapleader = " "
+vim.o.winborder = "rounded"
+vim.o.clipboard = "unnamedplus"
+
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
+vim.keymap.set('n', '<leader>w', ':write<CR>')
+vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
+
+vim.pack.add({
+		{ src = "https://github.com/catppuccin/nvim" },
+		{ src = "https://github.com/vague2k/vague.nvim" },
+		{ src = "https://github.com/stevearc/oil.nvim" },
+		{ src = "https://github.com/echasnovski/mini.pick" },
+		{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+		{ src = "https://github.com/neovim/nvim-lspconfig" },
+		{ src = "https://github.com/chomosuke/typst-preview.nvim" },
+		{ src = "https://github.com/mason-org/mason.nvim" },
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method('textDocument/completion') then
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		end
+	end,
+})
+vim.cmd("set completeopt+=noselect")
+
+require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+	ensure_installed = { "lua", "svelte", "typescript", "javascript", "html", "css" },
+	highlight = { enable = true }
+})
+require "oil".setup()
+
+vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
+vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
+vim.keymap.set('n', '<leader>e', ":Oil<CR>")
+vim.lsp.enable({ "lua_ls", "emmetls", "eslint_d" })
+
+require "catppuccin".setup({ transparent = true })
+vim.cmd("colorscheme catppuccin-mocha")
+vim.cmd(":hi statusline guibg=NONE")
